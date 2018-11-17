812e8721932e6a0b5bec386c6b277a319f949b10
Aaron Deimund <aaron@deimunddesigns.com>
2018-11-17 16:17:12 -0600

    Got Bable installed and configured

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import x from './test';
-
-console.log(`I imported ${x} from antoher module!`)
+const num = 23;
+console.log(`I imported ${x} from antoher module! Variable num is ${num}`);
